09f81ffd9bf36846bd060af5a7076cb210570606
MeerimI <irismamatovameerim@gmail.com>
2021-07-22 00:32:25 +0600

    first query

--- adapter/views.py
+++ adapter/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+import tinvest as ti
 
-# Create your views here.
+ 
+TOKEN = 't.Cz0mvF5Z-uPelMSg5eTTHTSe06y2E227cjLXqp09J4ZzjdFrsw7Mk1VG6fgiuE_iJWcPzYbNjpvB5LZUkIV92Q'
+ 
+# регистрируем аккаунт
+client = ti.SyncClient(TOKEN, use_sandbox=True)
+body = ti.SandboxRegisterRequest.tinkoff()
+response = client.register_sandbox_account(body)
+broker_account_id = response.payload.broker_account_id
+# print(response.payload)
+ 
+# получение списка акций
+response = client.get_market_stocks()
+print(response.payload)
