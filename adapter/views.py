674642fd4f0fc7d81907564d6d1aff262d942d6b
Dana <bakassarinad@gmail.com>
2021-07-23 19:01:41 +0600

    Added Market Detail

--- adapter/views.py
+++ adapter/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
 import tinvest as ti
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
  
 TOKEN = 't.Cz0mvF5Z-uPelMSg5eTTHTSe06y2E227cjLXqp09J4ZzjdFrsw7Mk1VG6fgiuE_iJWcPzYbNjpvB5LZUkIV92Q'
  
 # регистрируем аккаунт
-client = ti.SyncClient(TOKEN, use_sandbox=True)
-body = ti.SandboxRegisterRequest.tinkoff()
-response = client.register_sandbox_account(body)
-broker_account_id = response.payload.broker_account_id
-# print(response.payload)
- 
-# получение списка акций
-response = client.get_market_stocks()
-print(response.payload)
+class MarketDetail(APIView):
+    def get(self, request, ticker):
+    # регистрируем аккаунт
+        client = ti.SyncClient(TOKEN, use_sandbox=True)
+        body = ti.SandboxRegisterRequest.tinkoff()
+        register = client.register_sandbox_account(body)
+        response = client.get_market_search_by_ticker(ticker)
+        return Response(response)
