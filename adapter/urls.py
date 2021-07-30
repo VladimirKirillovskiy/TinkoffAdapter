243f9a615ace26363678a772d8231eb9c528b51a
Yegor <gare577@gmail.com>
2021-07-30 10:02:09 +0300

    Get market stocks view

--- adapter/urls.py
+++ adapter/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from adapter.views import MarketDetail
+from adapter.views import (MarketDetail, MarketAll)
 from rest_framework.authtoken.views import obtain_auth_token
 
 
 urlpatterns = [
     path('login/', obtain_auth_token, name='login'),
-    path('market/<str:ticker>', MarketDetail.as_view(), name='first')
+    path('market/all', MarketAll.as_view(), name='marketall'),
+    path('market/<str:ticker>', MarketDetail.as_view(), name='first'),
+    # path('market/whole', MarketWhole.as_view(), name='wholem'),
 ]
--- adapter/views.py
+++ adapter/views.py
@@ -1,4 +1,3 @@
-
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -15,4 +14,14 @@ class MarketDetail(APIView):
         client = ti.SyncClient(SANDBOX_TOKEN, use_sandbox=True)
         register = client.register_sandbox_account(ti.SandboxRegisterRequest.tinkoff())
         response = client.get_market_search_by_ticker(ticker)
+        return Response(json.loads(response.json()))
+
+
+class MarketAll(APIView):
+    permission_classes = [AllowAny,]
+
+    def get(self, request):
+        client = ti.SyncClient(SANDBOX_TOKEN, use_sandbox=True)
+        register = client.register_sandbox_account(ti.SandboxRegisterRequest.tinkoff())
+        response = client.get_market_stocks()
         return Response(json.loads(response.json()))
