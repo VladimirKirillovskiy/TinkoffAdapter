db1c61bdf70b857757433f6ea7e6ff2502bd557b
Yegor <gare577@gmail.com>
2021-07-30 10:04:48 +0300

    urls.py fix

--- adapter/urls.py
+++ adapter/urls.py
@@ -6,6 +6,5 @@ from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path('login/', obtain_auth_token, name='login'),
     path('market/all', MarketAll.as_view(), name='marketall'),
-    path('market/<str:ticker>', MarketDetail.as_view(), name='first'),
-    # path('market/whole', MarketWhole.as_view(), name='wholem'),
+    path('market/<str:ticker>', MarketDetail.as_view(), name='first')
 ]
