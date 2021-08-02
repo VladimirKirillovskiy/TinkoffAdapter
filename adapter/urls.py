fed4883a87d162a46c522183fb32c0aef815f721
Yegor <gare577@gmail.com>
2021-08-02 19:04:09 +0300

    getInsiders setup

--- adapter/urls.py
+++ adapter/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
-from adapter.views import MarketDetail
+from adapter.views import (MarketDetail, Insiders)
 from rest_framework.authtoken.views import obtain_auth_token
 
 
 urlpatterns = [
     path('login/', obtain_auth_token, name='login'),
     path('market/<str:ticker>', MarketDetail.as_view(), name='first')
+]
+
+urlpatterns = [
+    path('login/', obtain_auth_token, name='login'),
+    path('market/<str:ticker>', MarketDetail.as_view(), name='market'),
+    path('insiders/<str:ticker>&<int:days>', Insiders.as_view(), name='insiders'),
+    path('insiders/<str:ticker>', Insiders.as_view(), name='insidersT'),  # вызов за последние 7 дней по умол.
 ]
--- adapter/views.py
+++ adapter/views.py
@@ -1,4 +1,3 @@
-
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -6,6 +5,7 @@ from rest_framework.permissions import (IsAuthenticated, AllowAny)
 from TinkoffAdapter.settings import SANDBOX_TOKEN
 import tinvest as ti
 import json
+import adapter.services as services
  
 
 class MarketDetail(APIView):
@@ -16,3 +16,11 @@ class MarketDetail(APIView):
         register = client.register_sandbox_account(ti.SandboxRegisterRequest.tinkoff())
         response = client.get_market_search_by_ticker(ticker)
         return Response(json.loads(response.json()))
+
+
+class Insiders(APIView):
+    permission_classes = [AllowAny,]
+
+    def get(self, request, ticker, days=7):
+        data = services.get_insiders(ticker, days)
+        return Response(services.pd_insiders(data))
