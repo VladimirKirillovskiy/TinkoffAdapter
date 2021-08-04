503f71ec3137b493a117b3bee10ba7ab44d3b151
MeerimI <irismamatovameerim@gmail.com>
2021-08-05 01:00:56 +0600

    added currency req

--- adapter/urls.py
+++ adapter/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from adapter.views import MarketDetail
+from adapter.views import MarketDetail, Currencies
 from rest_framework.authtoken.views import obtain_auth_token
 
 
 urlpatterns = [
     path('login/', obtain_auth_token, name='login'),
-    path('market/<str:ticker>', MarketDetail.as_view(), name='first')
+    path('market/<str:ticker>', MarketDetail.as_view(), name='first'),
+    path('currency', Currencies.as_view(), name='currency')
 ]
--- adapter/views.py
+++ adapter/views.py
@@ -1,4 +1,3 @@
-
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -16,3 +15,22 @@ class MarketDetail(APIView):
         register = client.register_sandbox_account(ti.SandboxRegisterRequest.tinkoff())
         response = client.get_market_search_by_ticker(ticker)
         return Response(json.loads(response.json()))
+
+
+class Currencies(APIView):
+    permission_classes = [AllowAny,]
+    
+    def get(self, request):
+        client = ti.SyncClient(SANDBOX_TOKEN, use_sandbox=True)
+        register = client.register_sandbox_account(ti.SandboxRegisterRequest.tinkoff())
+        data = client.get_market_currencies().dict()['payload']['instruments']
+        data_json = []
+        for item in data:
+            res = {}
+            res['name'] = item['name']
+            res['ticker'] = item['ticker']
+            res['currency'] = item['currency']
+            res['figi'] = item['figi']
+            data_json.append(res)
+        return Response(json.dumps(data_json, ensure_ascii=False))
+
