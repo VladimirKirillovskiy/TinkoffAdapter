925280b8502fade5dbca59e06d00b98d5fa8128c
Carllosl <Carllosl@yandex.ru>
2021-08-04 18:38:40 +0300

    Add django requests
    Add currencies, stock info, dividends, earnings, next earnings

--- adapter/urls.py
+++ adapter/urls.py
@@ -1,9 +1,21 @@
 from django.urls import path
 from adapter.views import MarketDetail
+from adapter.views import MarketCurrencies
+from adapter.views import QuartEarnings
+from adapter.views import NextEarns
+from adapter.views import Info
+from adapter.views import Dividends
+from adapter.views import NextDivs
 from rest_framework.authtoken.views import obtain_auth_token
 
 
 urlpatterns = [
     path('login/', obtain_auth_token, name='login'),
+    path('market/currencies', MarketCurrencies.as_view(), name='currencies'),
+    path('stock/earn/<str:ticker_name>', QuartEarnings.as_view(), name='earnings'),
+    path('stock/nextearns/<str:ticker_name>', NextEarns.as_view(), name='next earnings'),
+    path('stock/info/<str:ticker_name>', Info.as_view(), name='info'),
+    path('stock/dividends/<str:ticker_name>', Dividends.as_view(), name='dividends'),
+    path('stock/nextdivs/<str:ticker_name>', NextDivs.as_view(), name='next dividends'),
     path('market/<str:ticker>', MarketDetail.as_view(), name='first')
 ]
--- adapter/views.py
+++ adapter/views.py
@@ -1,4 +1,3 @@
-
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -6,13 +5,119 @@ from rest_framework.permissions import (IsAuthenticated, AllowAny)
 from TinkoffAdapter.settings import SANDBOX_TOKEN
 import tinvest as ti
 import json
- 
+import yfinance as yf
+
 
 class MarketDetail(APIView):
-    permission_classes = [AllowAny,]
-    
+    permission_classes = [AllowAny, ]
+
     def get(self, request, ticker):
         client = ti.SyncClient(SANDBOX_TOKEN, use_sandbox=True)
         register = client.register_sandbox_account(ti.SandboxRegisterRequest.tinkoff())
         response = client.get_market_search_by_ticker(ticker)
         return Response(json.loads(response.json()))
+
+
+class MarketCurrencies(APIView):
+    permission_classes = [AllowAny, ]
+
+    def get(self, request):
+        client = ti.SyncClient(SANDBOX_TOKEN, use_sandbox=True)
+        register = client.register_sandbox_account(ti.SandboxRegisterRequest.tinkoff())
+        data = client.get_market_currencies().dict()['payload']['instruments']
+        data_json = []
+        for item in data:
+            res = {}
+            res['name'] = item['name']
+            res['ticker'] = item['ticker']
+            res['currency'] = item['currency']
+            data_json.append(res)
+        return Response(json.dumps(data_json, ensure_ascii=False))
+
+
+class QuartEarnings(APIView):
+    permission_classes = [AllowAny, ]
+
+    def get(self, request, ticker_name):
+        tick = yf.Ticker(ticker_name)
+        data = tick.quarterly_earnings
+        data_json = None
+        if data is not None:
+            data_json = data.to_json(orient="index")
+        return Response(data_json)
+
+
+class Info(APIView):
+    permission_classes = [AllowAny, ]
+
+    def get(self, request, ticker_name):
+        tick = yf.Ticker(ticker_name)
+        data = tick.info
+        data_json = None
+        if data is not None:
+            if data['trailingEps'] != None:
+                EPS = round(data['trailingEps'], 2)
+            else:
+                EPS = None
+            if data['beta'] != None:
+                Beta = round(data['beta'], 2)
+            else:
+                Beta = None
+            if data['debtToEquity'] != None:
+                DebtToEquity = round(data['debtToEquity'], 2)
+            else:
+                DebtToEquity = None
+            data_json = {
+                'MarketCap': data['marketCap'],
+                'SharesOutstanding': data['sharesOutstanding'],
+                'Revenue': data['totalRevenue'],
+                'EPS': EPS,
+                'Beta': Beta,
+                'Cash': data['totalCash'],
+                'Debt': data['totalDebt'],
+                'DebtToEquity': DebtToEquity
+            }
+        return Response(json.dumps(data_json))
+
+
+class Dividends(APIView):
+    permission_classes = [AllowAny, ]
+
+    def get(self, request, ticker_name):
+        tick = yf.Ticker(ticker_name)
+        data = tick.dividends
+        if data is not None:
+            data_json = data.to_json()
+        else:
+            data_json = None
+        return Response(data_json)
+
+
+class NextDivs(APIView):
+    permission_classes = [AllowAny, ]
+
+    def get(self, request, ticker_name):
+        tick = yf.Ticker(ticker_name)
+        data = tick.info
+        data_json = None
+        if data is not None:
+            time = data['exDividendDate']
+            data_json = {'next_div_day': time}
+        return Response(json.dumps(data_json))
+
+
+class NextEarns(APIView):
+    permission_classes = [AllowAny, ]
+
+    def get(self, request, ticker_name):
+        tick = yf.Ticker(ticker_name)
+        data = tick.calendar
+        data_json = None
+        if data is not None:
+            col = data.columns.values[1]
+            data_json = {
+                'Date': data.loc[data.index.values[0], col].value // 10 ** 9,
+                'EPS': data.loc[data.index.values[1], col],
+                'Revenue': data.loc[data.index.values[4], col]
+            }
+        return Response(json.dumps(data_json))
