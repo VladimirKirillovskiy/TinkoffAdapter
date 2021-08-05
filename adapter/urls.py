bb5f6f852056ca23cb22a374cd7e806cadaca8bf
Carllosl <Carllosl@yandex.ru>
2021-08-05 15:16:22 +0300

    Change imports

--- adapter/urls.py
+++ adapter/urls.py
@@ -1,21 +1,15 @@
 from django.urls import path
-from adapter.views import MarketDetail
-from adapter.views import MarketCurrencies
-from adapter.views import QuartEarnings
-from adapter.views import NextEarns
-from adapter.views import Info
-from adapter.views import Dividends
-from adapter.views import NextDivs
+import adapter.views as ad
 from rest_framework.authtoken.views import obtain_auth_token
 
 
 urlpatterns = [
     path('login/', obtain_auth_token, name='login'),
-    path('market/currencies', MarketCurrencies.as_view(), name='currencies'),
-    path('stock/earn/<str:ticker_name>', QuartEarnings.as_view(), name='earnings'),
-    path('stock/nextearns/<str:ticker_name>', NextEarns.as_view(), name='next earnings'),
-    path('stock/info/<str:ticker_name>', Info.as_view(), name='info'),
-    path('stock/dividends/<str:ticker_name>', Dividends.as_view(), name='dividends'),
-    path('stock/nextdivs/<str:ticker_name>', NextDivs.as_view(), name='next dividends'),
-    path('market/<str:ticker>', MarketDetail.as_view(), name='first')
+    path('market/currencies', ad.MarketCurrencies.as_view(), name='currencies'),
+    path('stock/earn/<str:ticker_name>', ad.QuartEarnings.as_view(), name='earnings'),
+    path('stock/nextearns/<str:ticker_name>', ad.NextEarns.as_view(), name='next earnings'),
+    path('stock/info/<str:ticker_name>', ad.Info.as_view(), name='info'),
+    path('stock/dividends/<str:ticker_name>', ad.Dividends.as_view(), name='dividends'),
+    path('stock/nextdivs/<str:ticker_name>', ad.NextDivs.as_view(), name='next dividends'),
+    path('market/<str:ticker>', ad.MarketDetail.as_view(), name='first')
 ]
